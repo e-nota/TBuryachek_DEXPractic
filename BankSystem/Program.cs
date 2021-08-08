0edf12b0f76a59dfbf8fd8584f8a22bd1dadc811
e-nota <tat.novak@gmail.com>
2021-08-08 23:11:44 +0300

    check

--- BankSystem/Program.cs
+++ BankSystem/Program.cs
@@ -10,12 +10,13 @@ namespace BankSystem
     {
         static void Main(string[] args)
         {
+            /*
             Client client1 = new Client() { Fio = "Ivanov", PassNum = "Q123" };
             Client client2 = new Client() { Fio = "Petrov", PassNum = "Q124" };
             Client client3 = new Client() { Fio = "Zaicev", PassNum = "Q125" };
             Client client4 = new Client() { Fio = "Volkova", PassNum = "Q126" };
             Client client5 = new Client() { Fio = "Ivanova", PassNum = "Q127" };
-
+            */
             MDL lei = new MDL() { Rate = 1.1 };
             RUB rubl = new RUB() { Rate = 0.2 };
             UAH grivna = new UAH() { Rate = 0.65 };
